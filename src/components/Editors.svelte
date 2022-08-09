34814b69bf27e1990f0e78088cae887372635076
andrewvallejo <andrew.a.vallejo@gmail.com>
2022-08-08 18:19:25 -0600

    feat(remake): Add live editor

---
+++ src/components/Editors.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+	let textInput: string = '';
+</script>
+
+<div class="editor">
+	<div class='container'>
+		<p class='live-text'>{textInput}</p>
+		<textarea class="text-input" bind:value={textInput}/>
+	</div>
+</div>
+
+<style lang="scss">
+	.editor {
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		display: flex;
+		width: 100%;
+		height: 100%;
+		.container {
+			position: relative;
+			width: 60%;
+			height: inherit;
+			.live-text {
+				padding: 0;
+				margin: 0;
+				position: absolute;
+				top: 0;
+				height: inherit;
+				z-index: 1;
+				max-width: 100%;
+				pointer-events: none;
+				word-wrap: break-word;
+				}
+			.text-input {
+				padding: 0;
+				margin: 0;
+				position: absolute;
+				top:0;
+				border: none;
+				width: 100%;
+				height: inherit;
+				outline: none;
+				border: 1px solid lightgray;
+				color: white;
+				caret-color: black;
+			}
+		}
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,61 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { base } from '$app/paths';
+	import Editors from '../components/Editors.svelte';
+
+	export let editor;
+
+	let editorState = editor;
+
+	const bark = () => {
+		console.log('woof');
+	};
+</script>
+
+<div class="wrapper">
+	<header>
+		<img class="logo" src={`${base}/favicon.png`} alt="Lettra's Logo" />
+		<div class="title">
+			<h1>Lettra</h1>
+			<h2>by Andrew Vallejo</h2>
+		</div>
+	</header>
+	<button on:click={bark}>Woof</button>
+	<Editors />
+</div>
+
+<style lang="scss">
+	:global(*) {
+		padding: 0;
+		margin: 0;
+    box-sizing: border-box;
+		font-family:  Roboto, sans-serif;
+    font-size: 1.5rem;
+    color: black;
+  }
+	.wrapper {
+		flex-direction: column;
+		align-items: center;
+		justify-content: space-around;
+		display: flex;
+		height: 100vh;
+	}
+
+	.logo {
+		width: 100px;
+		height: 100px;
+	}
+
+	header {
+		flex-direction: row;
+		align-items: center;
+		justify-content: space-around;
+		display: flex;
+		width: 100%;
+		line-height: 0.1;
+		.title {
+			flex-direction: column;
+			align-items: center;
+			display: flex;
+		}
+	}
+</style>
