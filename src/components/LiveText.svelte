2a16e7dd25b98f0f6cfa6a5a27fb76db2ee65855
andrewvallejo <andrew.a.vallejo@gmail.com>
2022-08-21 16:55:29 -0600

    chore(remake): clean files

--- src/components/LiveText.svelte
+++ src/components/LiveText.svelte
@@ -1,19 +1,17 @@
 <script lang="ts">
-  export let text:string = '';
+	export let text: string = '';
 </script>
 
 <p class="live-text">{@html text}</p>
 
 <style lang="scss">
-
-.live-text {
-				position: absolute;
-				z-index: 1;
-				top: 0;
-				pointer-events: none;
-				width: 100%;
-				height: inherit;
-				word-wrap: break-word;
-			}
-
+	.live-text {
+		position: absolute;
+		z-index: 1;
+		top: 0;
+		pointer-events: none;
+		width: 100%;
+		height: inherit;
+		word-wrap: break-word;
+	}
 </style>
