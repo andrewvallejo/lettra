476bf983b3ccd1ac2fafdc532483f32364976c4c
andrewvallejo <andrew.a.vallejo@gmail.com>
2022-09-24 14:06:18 -0600

    feat(objectify-words): move wordiable logic to derived words

--- src/stores/text.ts
+++ src/stores/text.ts
@@ -1,10 +1,13 @@
 import { derived, writable } from 'svelte/store';
 
-import { objectifyWord } from '$src/lib/editor';
+import { objectifyWord, checkForWordiables } from '$src/lib/editor';
 
 export const text = writable('');
 
-export const parsedText = derived(text, ($text) => objectifyWord($text));
+export const parsedText = derived(text, ($text) => {
+	checkForWordiables($text);
+	return objectifyWord($text);
+});
 
 export const wordiables: never = writable([]);
 
