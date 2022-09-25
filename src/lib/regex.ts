58c178d0653fe26effc4a077f95bb0ffbc709ca9
andrewvallejo <andrew.a.vallejo@gmail.com>
2022-09-25 15:24:45 -0600

    feat(sveltekit-typescript): add regex

--- src/lib/regex.ts
+++ src/lib/regex.ts
@@ -1,7 +1,8 @@
-import type { RegexI } from '$lib/types';
+import type { RegexI } from '$types';
 
 export const regex: RegexI = {
 	wordiables: /\\(.*?)\\/g,
 	backslash: /\\/g,
-	newline: /\n/g
+	newLine: /\n/g,
+	newTab: /\t/g
 };
