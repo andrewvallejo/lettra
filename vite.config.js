3b5a1473a0eb05f3dd43f4e38f79d3bafffc7eba
andrewvallejo <andrew.a.vallejo@gmail.com>
2022-09-25 15:24:04 -0600

    fix(sveltekit-typescript): fix type resolve

--- vite.config.js
+++ vite.config.js
@@ -7,11 +7,10 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	resolve: {
 		alias: {
-			$src: path.resolve('/src'),
 			$stores: path.resolve('/src/stores'),
 			$components: path.resolve('/src/components'),
 			$text: path.resolve('/src/components/editor/text'),
-			$types: path.resolve('/src/types'),
+			$types: path.resolve('/src/types.d.ts'),
 			$editor: path.resolve('/src/lib/editor')
 		}
 	}
