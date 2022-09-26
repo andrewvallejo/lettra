edde0c72ed790f9d4153302b97a04de320643ed4
andrewvallejo <andrew.a.vallejo@gmail.com>
2022-09-25 19:36:32 -0600

    refactor(sveltekit-typescript): added dynamic aspects

--- src/components/editor/text/Word.svelte
+++ src/components/editor/text/Word.svelte
@@ -1,15 +1,28 @@
 <script lang="ts">
+	import { space } from '$lib/editor/words';
 	import type { WordI } from '$types';
+	import { fade } from 'svelte/transition';
 
 	export let word: WordI;
 
 	let color: string = word.color;
-	let text = ` ${word.string}`;
+	let text: string = word.string;
+	let wordiable: boolean = word.isWordiable;
 </script>
 
-<span style:color>
-	{@html text}
-</span>
+{#if wordiable}
+	<span class:wordiable style:color transition:fade>
+		{@html space + text}
+	</span>
+{:else}
+	<span>
+		{@html space + text}
+	</span>
+{/if}
 
 <style lang="scss">
+	.wordiable {
+		position: relative;
+		z-index: 1;
+	}
 </style>
