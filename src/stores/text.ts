8a12b167cbfa74654562369a6979ee6c3cccc405
andrewvallejo <andrew.a.vallejo@gmail.com>
2022-09-26 20:25:01 -0600

    refactor(sveltekit-typescript): word modifications all now happen in parsedText

--- src/stores/text.ts
+++ src/stores/text.ts
@@ -1,6 +1,6 @@
 import { derived, writable } from 'svelte/store';
 import { objectifyWords, splitText, replaceNewlines } from '$editor/words';
-import { checkForWordiables } from '$editor/wordiables';
+import { checkForWordiables, powerWordiables } from '$editor/wordiables';
 import type { WordI } from '$types';
 import type { Writable } from 'svelte/store';
 
@@ -8,8 +8,12 @@ export const text: Writable<string> = writable('');
 
 export const parsedText = derived(text, ($text): WordI[] => {
 	checkForWordiables($text);
-	const words = splitText(replaceNewlines($text));
-	return objectifyWords(words);
+	let words = splitText(replaceNewlines($text));
+	// Words need to have properties isSpace or isLineBreak added.
+	// Need to make sure space is its own word
+	words = objectifyWords(words);
+	powerWordiables(words);
+	return words;
 });
 
 export const wordiables: Writable<string[]> = writable([]);
