b8ba1e1b4685a5b947d755b034a672f1f0eeeb44
andrewvallejo <andrew.a.vallejo@gmail.com>
2022-09-27 14:48:45 -0600

    fix(sveltekit-typescript): add abs pos to prevent web flow conflicts

--- src/components/Table.svelte
+++ src/components/Table.svelte
@@ -2,8 +2,17 @@
 	import { wordiables } from '$stores/text';
 </script>
 
-<div>
+<div class="wordiables">
 	{#each $wordiables as wordiable}
 		<p>{wordiable}</p>
 	{/each}
 </div>
+
+<style lang="scss">
+	.wordiables {
+		position: absolute;
+		z-index: 2;
+		display: flex;
+		flex-direction: column;
+	}
+</style>
