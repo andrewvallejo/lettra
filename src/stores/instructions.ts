7004416e06ad69dc679167906869aafde9f67538
andrewvallejo <andrew.a.vallejo@gmail.com>
2022-10-05 09:33:45 -0600

    Update naming conventions

--- src/stores/instructions.ts
+++ src/stores/instructions.ts
@@ -1,18 +1,19 @@
 import { writable } from 'svelte/store';
 
 type Instructions = {
-	prompt: string;
-	active: boolean;
+	instruction: string;
+	instructionActive: boolean;
 };
 
 const store: Instructions = {
-	prompt: `In order to create a wordiable, you must surround it with two backslashes. For Example, \\flamingo\\ and \\doctor\\ could be wordiables. With every following iteration of the word's colors corresponding with it. \n  \n  I am applying for the \\doctor\\ role at \\flamingo\\ inc. \n \n	Hit any key to clear editor and begin!`,
-	active: true
+	instruction: `In order to create a wordiable, you must surround it with two backslashes. For Example, \\flamingo\\ and \\doctor\\ could be wordiables. With every following iteration of the word's colors corresponding with it. \n  \n  I am applying for the \\doctor\\ role at \\flamingo\\ inc. \n \n	Hit any key to clear editor and begin!`,
+	instructionActive: true
 };
 
 const instructionsStore = () => {
 	const { subscribe, set, update } = writable(store);
-	const deactivate = () => update((instructions) => ({ ...instructions, active: false }));
+	const deactivate = () =>
+		update((instructions) => ({ ...instructions, instructionActive: false }));
 
 	return {
 		subscribe,
