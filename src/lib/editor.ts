c86dd796872f6d23e935114c881d59eb309d91b7
andrewvallejo <andrew.a.vallejo@gmail.com>
2022-10-06 16:21:57 -0600

    Combine files

--- src/lib/editor.ts
+++ src/lib/editor.ts
@@ -26,3 +26,29 @@ export const powerWordiables = (text: Word[], preWordiables: string[]): void => 
 		}
 	});
 };
+
+export const replaceNewlines = (text: string): string => {
+	return splitText(text.replace(regex.newLine, ' <br> '));
+};
+
+export const removeBackSlash = (word: string) => {
+	return word.replace(/\\/g, '');
+};
+
+export const space = ' ';
+
+export const splitText = (text: string): string[] => text.split(' ');
+
+export const objectifyWords = (words: string[], preWordiables: string[]): Word[] => {
+	return words.map((word: string, index: number): Word => {
+		return {
+			string: word,
+			index,
+			isWordiable: isWordiable(word, preWordiables),
+			color: 'black',
+			wordiablePos: -1,
+			occurrences: 0,
+			type: isWordiable ? 'wordiable' : 'word'
+		};
+	});
+};
