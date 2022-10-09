3b61f229d368fe58bf80d51f8422bb981ca8ab78
andrewvallejo <andrew.a.vallejo@gmail.com>
2022-10-08 18:51:22 -0600

    Optimize and clean code

--- src/lib/editor.ts
+++ src/lib/editor.ts
@@ -1,19 +1,19 @@
 import type { Word } from '$types';
-import { regex } from './regex';
 import { rainbow } from './strings';
+import { regex } from './regex';
 
-export const isWordiable = (word: string, wordiableDraft: string[]): boolean => {
+export const isWordiable = (word: string): boolean => {
 	const match = word.match(regex.wordiables);
-	if (wordiableDraft.includes(word) || match) return true;
+	if (match) return true;
 	return false;
 };
 
-export const objectifyWords = (words: string[], wordiableDraft: string[]): Word[] => {
+export const objectifyWords = (words: string[]): Word[] => {
 	return words.map((word: string, index: number): Word => {
 		return {
 			string: word,
 			index,
-			isWordiable: isWordiable(word, wordiableDraft),
+			isWordiable: isWordiable(word),
 			color: 'black',
 			wordiablePos: -1,
 			occurrences: 0,
@@ -22,10 +22,11 @@ export const objectifyWords = (words: string[], wordiableDraft: string[]): Word[
 	});
 };
 
-export const powerWordiables = (text: Word[], wordiableDraft: string[]): void => {
+export const powerWordiables = (text: Word[], wordiableDraft: Word[]): void => {
+	const stringifiedDraft = wordiableDraft.map((word) => word.string);
 	text.forEach((word) => {
 		if (word.isWordiable) {
-			word.wordiablePos = wordiableDraft.indexOf(word.string);
+			word.wordiablePos = stringifiedDraft.indexOf(word.string);
 			word.color = rainbow[word.wordiablePos];
 			word.type = 'wordiable';
 		}
