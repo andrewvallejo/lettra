701a90d8979a7907fcc1a3ed52a062eae5d38e49
andrewvallejo <andrew.a.vallejo@gmail.com>
2022-10-10 14:04:26 -0600

    Fix logic so buttons work everywhere

--- src/components/LiveWord.svelte
+++ src/components/LiveWord.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
-<<<<<<< HEAD
-	import { addBackSlashes, reverseParseText, rainbow, removeBackSlashes } from '$lib/strings';
+	import { addBackSlashes, rainbow, removeBackSlashes, reverseParseText } from '$lib/strings';
 	import { cleanText, text as textStore } from '$stores/text';
-=======
-	import { rainbow } from '$lib/strings';
->>>>>>> 78872bb (Revert "Feature/button")
 	import type { Word } from '$types';
 	import { interpolateLab } from 'd3-interpolate';
 	import { tweened, type Tweened } from 'svelte/motion';
 
 	export let word: Word;
 
+	let wordType: string = word.type;
+
 	let text: string = word.string;
+
+	let index = word.index;
 
 	let color: Tweened<string> = tweened(rainbow[7], {
 		duration: 350,
@@ -20,7 +20,6 @@
 	});
 
 	const handleClick = () => {
-<<<<<<< HEAD
 		let newText: string;
 		let splitted: string[] = $cleanText;
 		let flippedWord: string =
@@ -29,9 +28,6 @@
 		splitted[index] = flippedWord;
 		newText = splitted.join(' ');
 		textStore.set(reverseParseText(newText));
-=======
-		console.log('button works');
->>>>>>> 78872bb (Revert "Feature/button")
 	};
 
 	$: color.set(rainbow[word.wordiablePos]);
