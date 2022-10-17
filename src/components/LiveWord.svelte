ac6012326ffd4924e99a53229b392e32e73358a2
andrewvallejo <andrew.a.vallejo@gmail.com>
2022-10-17 10:26:58 -0400

    Add input forms to customize wordiables

--- src/components/LiveWord.svelte
+++ src/components/LiveWord.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { addBackSlashes, rainbow, removeBackSlashes, reverseParseText } from '$lib/strings';
+	import { addBackSlashes, flipWord, rainbow, reverseParseText } from '$lib/strings';
 	import { cleanText, text as textStore } from '$stores/text';
 	import type { Word } from '$types';
 	import { interpolateLab } from 'd3-interpolate';
@@ -7,15 +7,25 @@
 
 	export let word: Word;
 
-	let active = false;
+	export let textArea: HTMLTextAreaElement;
 
-	let wordType: string = word.type;
+	let { index, string, type } = word;
 
-	let text: string = word.string;
+	let flippedWord: string = flipWord(string, type);
 
-	let index = word.index;
+	let isSelected: boolean = false;
 
-	let color: Tweened<string> = tweened(rainbow[7], {
+	let isTextMenuActive: boolean = false;
+
+	let button: HTMLSpanElement;
+
+	let htmlInput: HTMLInputElement;
+
+	let value: string = string;
+
+	let inputValue: string;
+
+	const color: Tweened<string> = tweened(rainbow[7], {
 		duration: 350,
 		delay: 250,
 		interpolate: interpolateLab
@@ -23,43 +33,96 @@
 
 	const debounce = (callback: () => void) => {
 		setTimeout(() => {
-			if (!active) {
+			if (!isSelected) {
 				callback();
 			}
 		}, 250);
 	};
 
 	const handleClick = () => {
-		let newText: string;
 		let splitted: string[] = $cleanText;
-		let flippedWord: string =
-			wordType === 'wordiable' ? removeBackSlashes(word.string) : addBackSlashes(word.string);
+		let newText: string;
+		textArea.focus();
+
 		splitted[index] = flippedWord;
 		newText = reverseParseText(splitted.join(' '));
 		textStore.set(newText);
 	};
 
 	const handleDoubleClick = () => {
-		active = true;
+		isSelected = true;
+		isTextMenuActive = true;
+		htmlInput && selectText();
+
 		setTimeout(() => {
-			if (active) {
-				active = false;
+			if (isSelected) {
+				isSelected = false;
 			}
 		}, 250);
 	};
 
+	const handleFocus = () => {
+		setTimeout(() => {
+			button.focus();
+		}, 0);
+	};
+
+	const selectText = () => {
+		let start = $cleanText.indexOf(string);
+		let end = start + string.length;
+
+		textArea.focus();
+		textArea.setSelectionRange(start + 1, end - 1);
+		htmlInput.setSelectionRange(start + 1, end - 1);
+	};
+
+	// Create a store that stores the correct form of Text String
+	// TODO: fix double click
+	// TODO: when hitting enter; the cursor should be at the end of the sentence
+	// TODO: When you click on a word while isTextMenuActive is true, it should enter the word via handleInpt
+
+	const handleInput = (event: any) => {
+		if (event.key === 'Enter') {
+			event.preventDefault();
+			let splitted: string[] = $cleanText;
+			splitted[index] = inputValue;
+			textStore.set(reverseParseText(splitted.join(' ')));
+			isTextMenuActive = false;
+			isSelected = false;
+			textArea.setSelectionRange($textStore.length, $textStore.length);
+		} else if (event.key === 'Escape') {
+			event.preventDefault();
+			isTextMenuActive = false;
+			isSelected = false;
+			textArea.setSelectionRange($textStore.length, $textStore.length);
+		}
+	};
+
 	$: color.set(rainbow[word.wordiablePos]);
+	$: type === 'word' ? (inputValue = addBackSlashes(value)) : (inputValue = value);
 </script>
 
 <span>
-	<button
-		on:dblclick={handleDoubleClick}
-		on:click={() => debounce(handleClick)}
-		style="color: {$color}"
-		class={word.type}
-	>
-		{text}
-	</button>
+	{#if isTextMenuActive}
+		<input
+			bind:this={htmlInput}
+			bind:value
+			type="text"
+			on:keydown={handleInput}
+			style="width: {value.length}ch;"
+		/>
+	{:else}
+		<button
+			on:dblclick={handleDoubleClick}
+			on:click={() => debounce(handleClick)}
+			style="color: {$color}"
+			class={word.type}
+			bind:this={button}
+			on:focus={handleFocus}
+		>
+			{string}
+		</button>
+	{/if}
 </span>
 
 <style lang="scss">
@@ -83,6 +146,15 @@
 				transition: all 1s;
 				transform: scale(1.05);
 			}
+		}
+
+		input {
+			top: 0;
+			border: none;
+			background-image: none;
+			background-color: transparent;
+			box-shadow: none;
+			cursor: pointer;
 		}
 	}
 </style>
