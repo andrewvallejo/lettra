939b3d890476ccea022cbcbe3e386bc9ccad2092
andrewvallejo <andrew.a.vallejo@gmail.com>
2022-10-20 13:00:41 -0400

    Refactor logic to detect repeating words

--- src/lib/editor.ts
+++ src/lib/editor.ts
@@ -2,22 +2,23 @@ import type { Word } from '$types';
 import { addBackSlashes, rainbow } from './strings';
 import { regex } from './regex';
 
-export const isWordiable = (word: string, draft: string[] = []): boolean => {
+export const isWordiable = (word: string): boolean => {
 	const match = word.match(regex.wordiables);
 	if (match) return true;
-	if (draft.length) {
-		//check to see if word wrapped in \\ backslashes is in draft
-		if (draft.includes(addBackSlashes(word))) return true;
-	}
 	return false;
 };
 
-export const objectifyWords = (words: string[], wordiableDraft: Word[]): Word[] => {
+export const isInWordiables = (word: string, wordiables: string[] = []): boolean => {
+	if (wordiables.includes(addBackSlashes(word))) return true;
+};
+
+export const objectifyWords = (words: string[], wordiables: Word[]): Word[] => {
 	return words.map((word: string, index: number): Word => {
 		return {
 			string: word,
 			index,
-			isWordiable: isWordiable(word, wordiableDraft),
+			isWordiable: isWordiable(word, wordiables),
+			isInWordiables: isInWordiables(word, wordiables),
 			color: 'black',
 			wordiablePos: -1,
 			occurrences: 0,
@@ -26,11 +27,14 @@ export const objectifyWords = (words: string[], wordiableDraft: Word[]): Word[] 
 	});
 };
 
-export const powerWordiables = (text: Word[], wordiableDraft: Word[]): void => {
-	const stringifiedDraft = wordiableDraft.map((word) => word.string);
+export const powerWordiables = (text: Word[], wordiables: Word[]): void => {
 	text.forEach((word) => {
 		if (word.isWordiable) {
-			word.wordiablePos = stringifiedDraft.indexOf(word.string);
+			word.wordiablePos = wordiables.indexOf(word.string);
+			word.color = rainbow[word.wordiablePos];
+			word.type = 'wordiable';
+		} else if (word.isInWordiables) {
+			word.wordiablePos = wordiables.indexOf(addBackSlashes(word.string));
 			word.color = rainbow[word.wordiablePos];
 			word.type = 'wordiable';
 		}
