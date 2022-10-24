b14b7538e9ff43f6cc043baff1e72606a776eca9
andrewvallejo <andrew.a.vallejo@gmail.com>
2022-10-24 17:52:25 -0600

    Remove duplicates from merge

--- src/lib/strings.ts
+++ src/lib/strings.ts
@@ -10,8 +10,6 @@ export const space = ' ';
 
 export const splitText = (text: string): string[] => text.split(' ');
 
-export const rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet', 'black'];
-
 export const replaceLinebreaks = (text: string[]): string =>
 	text.reduce((acc, word) => {
 		if (word === '<br>') acc += '\n';
@@ -23,12 +21,6 @@ export const replaceNewlines = (text: string): string[] =>
 	splitText(text.replace(regex.newLine, ' <br> '));
 
 export const removeBackSlashes = (word: string) => word.replace(/\\/g, '');
-
-export const addBackSlashes = (text: string): string => text.replace(regex.word, '\\$&\\');
-
-export const splitText = (text: string): string[] => text.split(' ');
-
-export const space = ' ';
 
 export const rainbow = [
 	'rgb(255, 0, 0)',
